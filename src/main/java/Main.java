455e1f5c59d03038f457dc8aaa9ce9b5b643ff4e
Joe Kutner <jpkutner@gmail.com>
2016-04-06 09:52:45 -0500

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    (new Main()).get("https://maushaus.party", new HashMap<String, String>());
+  }
+
+  public static String get(String urlStr, Map<String,String> headers) throws IOException {
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    HttpGet request = new HttpGet(urlStr);
+    for (String key : headers.keySet()) {
+      String value = headers.get(key);
+      request.setHeader(key, value);
+    }
+    CloseableHttpResponse response = httpClient.execute(request);
+    try {
+      return handleResponse(response, Map.class);
+    } finally {
+      response.close();
+    }
+  }
+
+  private static String handleResponse(CloseableHttpResponse response, Class returnType) throws IOException {
+    StatusLine statusLine = response.getStatusLine();
+    HttpEntity entity = response.getEntity();
+    if (statusLine.getStatusCode() >= 300) {
+      throw new HttpResponseException(
+          statusLine.getStatusCode(),
+          statusLine.getReasonPhrase());
+    }
+    if (entity == null) {
+      throw new ClientProtocolException("Response contains no content");
+    }
+    return readStream(entity.getContent());
+  }
+
+  private static String readStream(InputStream is) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    String output = "";
+    String tmp = reader.readLine();
+    while (tmp != null) {
+      output += tmp;
+      tmp = reader.readLine();
+    }
+    return output;
+  }
+}
