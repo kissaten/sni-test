1d725e9985085c9e77ff06f67f31f1a960d26a75
Joe Kutner <jpkutner@gmail.com>
2016-04-06 10:45:58 -0500

    new test

---
+++ src/main/java/JavaNetSslExample.java
@@ -0,0 +1,59 @@
+import java.net.URL;
+import java.io.*;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.*;
+
+public class JavaNetSslExample
+{
+  private static SSLContext getWeakSSLContext() throws Exception {
+    // Create a trust manager that does not validate certificate chains
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+      }
+
+      @Override
+      public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException { }
+
+      @Override
+      public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException { }
+    }};
+
+    // Ignore differences between given hostname and certificate hostname
+    HostnameVerifier hv = new HostnameVerifier() {
+      @Override
+      public boolean verify(String hostname, SSLSession session) {
+        return true;
+      }
+    };
+
+    // Install the all-trusting trust manager
+    SSLContext sc = SSLContext.getInstance("TLSv1.2");
+    sc.init(null, trustAllCerts, new SecureRandom());
+    return sc;
+  }
+
+  public static void main(String[] args) throws Exception
+  {
+    SSLContext sc = getWeakSSLContext();
+    SSLContext.setDefault(sc);
+
+    String httpsURL = "https://maushaus.party/";
+    URL myurl = new URL(httpsURL);
+    HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+    InputStream ins = con.getInputStream();
+    InputStreamReader isr = new InputStreamReader(ins);
+    BufferedReader in = new BufferedReader(isr);
+
+    String inputLine;
+
+    while ((inputLine = in.readLine()) != null)
+    {
+      System.out.println(inputLine);
+    }
+
+    in.close();
+  }
+}
