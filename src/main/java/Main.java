e3df8c3d00c25c26e8361923cb0ae497871d655a
Joe Kutner <jpkutner@gmail.com>
2016-04-06 10:35:22 -0500

    sni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,52 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+import javax.net.ssl.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
+  private static SSLContext getWeakSSLContext() throws Exception {
+    // Create a trust manager that does not validate certificate chains
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+      }
+
+      @Override
+      public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException { }
+
+      @Override
+      public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException { }
+    }};
+
+    // Ignore differences between given hostname and certificate hostname
+    HostnameVerifier hv = new HostnameVerifier() {
+      @Override
+      public boolean verify(String hostname, SSLSession session) {
+        return true;
+      }
+    };
+
+    // Install the all-trusting trust manager
+    SSLContext sc = SSLContext.getInstance("TLSv1.2");
+    sc.init(null, trustAllCerts, new SecureRandom());
+    return sc;
+  }
+
   public static void main(String[] args) throws Exception {
-    String out = (new Main()).get("https://maushaus.party", new HashMap<String, String>());
+    SSLContext sc = getWeakSSLContext();
+    SSLContext.setDefault(sc);
+
+    String out = (new Main()).get("https://maushaus.party/", new HashMap<String, String>());
     System.out.println(out);
   }
 
